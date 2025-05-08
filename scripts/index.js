a6b51f53b2cc70a1bed58b516ad8c85eeacdcdbc
phillip <pilotophillip@gmail.com>
2025-05-08 13:25:40 -0700

    Project4 Stage3 Part 2 Completed

--- scripts/index.js
+++ scripts/index.js
@@ -1,13 +1,26 @@
 const editProfileBtn = document.querySelector(".profile__edit-btn");
 const editProfileModal = document.querySelector("#edit-profile-modal");
 const editProfileCloseBtn = document.querySelector(".modal__close-btn");
+const editProfileForm = editProfileModal.querySelector(".modal__form")
+const editProfileNameInput = editProfileModal.querySelector(
+    "#profile-name-input"
+);
+const editProfileDescriptionInput = editProfileModal.querySelector(
+    "#profile-description-input"
+);
 
-const newPostBtn = document.querySelector(".profile__new-post-btn");
+const newPostBtn = document.querySelector(".profile__add-btn");
 const newPostModal = document.querySelector("#new-post-modal");
 const newPostCloseBtn = newPostModal.querySelector(".modal__close-btn");
 
 
+const profileNameEl = document.querySelector(".profile__name");
+const profileDescriptionEl = document.querySelector(".profile__description");
+
+
 editProfileBtn.addEventListener("click", function (){
+  editProfileNameInput.value = profileNameEl.textContent;
+  editProfileDescriptionInput.value = profileDescriptionEl.textContent;  
   editProfileModal.classList.add("modal_is-opened");
 });
 
@@ -22,3 +35,37 @@ newPostBtn.addEventListener("click", function () {
 newPostCloseBtn.addEventListener("click", function () {
   newPostModal.classList.remove("modal_is-opened");
 });
+
+function handleEditProfileSubmit(evt){
+    evt.preventDefault();
+    profileNameEl.textContent = editProfileNameInput.value;
+    editProfileModal.classList.remove("modal_is-opened");
+    
+}
+
+editProfileForm.addEventListener("submit", handleEditProfileSubmit)
+
+
+// for new post
+
+
+
+const addCardFormElement = newPostModal.querySelector(".modal__form");
+const nameInput = addCardFormElement.querySelector("#profile-caption-input");
+const linkInput = addCardFormElement.querySelector("#new-card-image-input");
+
+// Create the form submission handler.
+function handleAddCardSubmit(evt) {
+  // Prevent default form submission behavior.
+  evt.preventDefault();
+  
+  // Log both input values to the console.
+  console.log("Caption: ", nameInput.value);
+  console.log("Image Link: ", linkInput.value);
+
+  // Close the modal.
+  newPostModal.classList.remove("modal_is-opened");
+}
+
+// Add the event listener for form submission.
+addCardFormElement.addEventListener('submit', handleAddCardSubmit);
