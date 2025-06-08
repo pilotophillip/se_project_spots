2586e2894c003c80c3b4f8185bbc4a8045108eda
phillip <pilotophillip@gmail.com>
2025-06-08 02:29:30 -0700

    Finished Project 5 stage 3 part 2 generating the cards from a template

--- scripts/index.js
+++ scripts/index.js
@@ -44,6 +44,23 @@ const newPostCloseBtn = newPostModal.querySelector(".modal__close-btn");
 const profileNameEl = document.querySelector(".profile__name");
 const profileDescriptionEl = document.querySelector(".profile__description");
 
+const cardTemplate =document.querySelector ("#card-template")
+.content.querySelector(".card");
+const cardsList = document.querySelector(".cards__list")
+
+function getCardElement(data) {
+  const cardElement = cardTemplate.cloneNode(true);
+  const cardTitleEl = cardElement.querySelector(".card__title"); 
+  const cardImageEl = cardElement.querySelector(".card__image");
+
+
+  cardImageEl.src = data.link;
+  cardImageEl.alt = data.name;
+  cardTitleEl.TextContent = data.name;
+ 
+  return cardElement;
+}
+
 function openModal (modal){
   modal.classList.add("modal_is-opened");
 }
@@ -116,6 +133,6 @@ function handleAddCardSubmit(evt) {
 addCardFormElement.addEventListener('submit', handleAddCardSubmit);
 
 intialCards.forEach(function (item) {
-  console.log(item.name); 
-  console.log(item.link); 
+  const cardElement = getCardElement(item); 
+cardsList.append(cardElement)
 });
